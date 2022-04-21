4f39a5e155876907f4c3042099e74b073e7c77ba
yopiti <david.klement@gmail.com>
2022-04-21 14:13:14 +0200

    Integrating Command Line Arguments
    Optional -l for language and -f for file.

--- main.py
+++ main.py
@@ -1,21 +1,39 @@
 import polib
 import deepl
+import getopt
+import sys
 
 DEEPL_API_TOKEN = 'ADD YOUR API KEY HERE!'
+
+global argv
+global opts
+global args
+
+argv = sys.argv[1:]
+opts, args = getopt.getopt(argv, "f:l:")
 
 def translate(text, lang):
     translator = deepl.Translator(DEEPL_API_TOKEN)
     return str(translator.translate_text(text, target_lang=lang))
 
-
 def get_filename():
-    print('Please enter the filename of the PO file e.g. /directory/django.po:')
-    filename = input()
+    # read arguments from command line
+    for opt, arg in opts:
+        if opt in ['-f']:
+            filename = arg
+    if not filename:
+            print('Please enter the filename of the PO file e.g. /directory/django.po:')
+            filename = input()
     return filename
 
 def get_target_language():
-    print('Please enter two letter ISO language code e.g. DE:')
-    lang = input()
+    # read arguments from command line
+    for opt, arg in opts:
+        if opt in ['-l']:
+            lang = arg
+    if not lang:
+            print('Please enter two letter ISO language code e.g. DE:')
+            lang = input()
     return lang
 
 def process_file(filename, lang):
